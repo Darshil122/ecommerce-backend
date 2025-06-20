4d8ff62be0ce97c6220a790da132db4e5582a1c3
Darshil122 <darshildabhi2005@gmail.com>
2025-06-20 18:47:57 +0530

    update file

--- index.js
+++ index.js
@@ -33,11 +33,13 @@ app.post("/login", async (req, res) => {
     users.push(newUser);
 
     try {
-      fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
-      return res.status(201).json({ message: "User created successfully" });
-    } catch (err) {
-      return res.status(500).json({ message: "Failed to save user" });
-    }
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+  return res.status(201).json({ message: "User created successfully" });
+} catch (err) {
+  console.error("Write Error:", err); // 👈 add this
+  return res.status(500).json({ message: "Failed to save user" });
+}
+
   }
 
   if (action === "Login") {
