63e89b5d38d4d7200070846a36dbb7e8d5f259f8
Darshil122 <darshildabhi2005@gmail.com>
2025-06-20 18:28:55 +0530

    Update file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(__dirname, "users.json");
+// const filePath = path.join(__dirname, "users.json");
 
 app.use(express.json());
 app.use(cors());
@@ -19,32 +19,22 @@ app.use(cors());
 app.post("/login", async (req, res) => {
   console.log("req body", req.body);
   const { name, email, password, action } = req.body;
-  if (action === "Sign Up") {
-    const existingUser = users.find(user => user.email === email);
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-    const newUser = { name, email, password };
-    users.push(newUser);
-    // const hashedPassword = await bcrypt.hash(password, 10);
-    // await User.create({ name, email, password, action });
-    // res.status(201).json({ message: "User created" });
-
-    try {
-      fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
-      return res.status(201).json({ message: "User created successfully" });
-    } catch (err) {
-      return res.status(500).json({ message: "Failed to save user" });
-    }
-  }
+  // if (action === "Sign Up") {
+  //   const existingUser = await User.findOne({ email });
+  //   if (existingUser) {
+  //     return res.status(400).json({ message: "User already exists" });
+  //   }
+  //   // const hashedPassword = await bcrypt.hash(password, 10);
+  //   await User.create({ name, email, password, action });
+  //   res.status(201).json({ message: "User created" });
+  // }
   if (action === "Login") {
     // const user = await User.findOne({ email });
-    const user = users.find(user => user.email === email && user.password === password);
 
-    if (user) {
-      return res.status(200).json({ message: "Login successful" });
+    if (email === "jaydev@gmail.com" || password === "jk@123") {
+      return res.status(200).json({ message: "Login successfully" });
     } else {
-      return res.status(400).json({ message: "Invalid email or password" });
+      return res.status(400).json({ message: "User not found" });
     }
     // const isMatch = await compare();
     // if (!password)
